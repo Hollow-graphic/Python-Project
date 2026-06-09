b2d5d2050b0895a48e8e013e57329b6117113c74
Hollow-graphic <120960592+Hollow-graphic@users.noreply.github.com>
2026-06-09 18:10:40 +0200

    Delete CelesteRandomArchipelago directory

--- CelesteRandomArchipelago/static/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", async (e) => {
-  const btn = e.target.closest("button");
-
-  if (!btn) return;
-
-  if (btn.textContent.includes("Clear")) {
-    await fetch("./api/clear", { method: "POST" });
-  }
-});
